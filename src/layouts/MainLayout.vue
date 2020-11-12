0792b5e82433e3eeff29f782d389dac210ba4e38
itsviejon <dcalflo8@gmail.com>
2020-11-12 12:40:53 -0600

    Tablero KANBAN/Menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,17 +5,33 @@
         <q-btn
           flat
           dense
-          round
           icon="menu"
           aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
-
         <q-toolbar-title>
-          Quasar App
+          Tablero Kanban
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <div class="row">
+        <h5>Proyecto: </h5>
+        <q-input v-model="text" :dense="dense" disable class="menu" />
+        </div>
+        <div class="row">
+        <h5>Fecha Estatus: </h5>
+        <q-input v-model="text" :dense="dense" disable class="menu"/>
+        </div>
+        <div class="row">
+        <h5>Product Owner: </h5>
+        <q-input v-model="text" :dense="dense" disable class="menu"/>
+        </div>
+        <div class="row">
+        <h5>Scrum Master: </h5>
+        <q-input v-model="text" :dense="dense" disable class="menu"/>
+        </div>
+        <div class="row">
+        <h5>Estatus: </h5>
+        <q-input v-model="text" :dense="dense" disable class="menu"/>
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -25,19 +41,71 @@
       bordered
       content-class="bg-grey-1"
     >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
+    <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+      <q-list padding class="menu-list">
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="table_view" />
+          </q-item-section>
+
+          <q-item-section>
+            Tablero Kanban
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="event_available" />
+          </q-item-section>
+
+          <q-item-section>
+            Proyecto
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="list_alt" />
+          </q-item-section>
+
+          <q-item-section>
+            Product Backlog
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="people" />
+          </q-item-section>
+
+          <q-item-section>
+            Scrum Team
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="poll" />
+          </q-item-section>
+
+          <q-item-section>
+            Informes
+          </q-item-section>
+        </q-item>
+          </q-list>
+          </q-scroll-area>
+        <q-img
+          class="absolute-top"
+          src="https://cdn.quasar.dev/img/material.png"
+          style="height: 150px"
         >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+            <div class="text-weight-bold">Enrique Flores</div>
+
+          </div>
+        </q-img>
     </q-drawer>
 
     <q-page-container>
@@ -47,60 +115,12 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
+import '../css/estilos.css'
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      leftDrawerOpen: false
     }
   }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+  <q-page class="">
+    <div class="row">
+      <div class="col-md-12">
+         <q-table
+         :data='rows'
+          title="Tablero KANBAN"
+          row-key="name"
+          />
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script></script>
+<script>
+var timeElapsed = Date.now()
+var today = new Date(timeElapsed)
+import '../css/estilos.css'
+export default {
+  name: 'Index',
+  data () {
+    return {
+      rows: [
+        {
+          name: 'SIAN',
+          prioridad: 'Alta',
+          estatus: 'Alta',
+          fechaEstatus: today.toLocaleDateString(),
+          development: 'Diego Calderón'
+        }
+      ]
+    }
+  }
+}
+</script>
